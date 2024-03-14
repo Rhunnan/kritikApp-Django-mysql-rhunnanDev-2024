f53b08cf99a40314d3b59f7a0f94ed2093b13b64
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-03-15 01:14:27 +0800

    adding futureprovider in initializehive file to fetch data

--- kritik_version_2023/lib/components/initiliazeHive.dart
+++ kritik_version_2023/lib/components/initiliazeHive.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kritik_version_2023/components/classEstablishment.dart';
 import 'package:kritik_version_2023/components/establishment_data_grid.dart';
 import 'package:kritik_version_2023/components/services.dart';
+import 'package:http/http.dart' as http;
 
 Future<void> initHive() async {
   final EstablishmentService establishmentServ = EstablishmentService();
@@ -30,3 +34,14 @@ Future<void> initHive() async {
     ], []));
   }
 }
+
+final dataProvider = FutureProvider<List<dynamic>>((ref) async {
+  final response = await http
+      .get(Uri.parse("http://192.168.254.102:8000/api/v1/establishment/"));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to Load data');
+  }
+});
--- kritik_version_2023/lib/main.dart
+++ kritik_version_2023/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:kritik_version_2023/components/book_mark_page.dart';
 import 'package:kritik_version_2023/components/classEstablishment.dart';
@@ -14,7 +15,10 @@ void main() async {
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(ReviewsAdapter());
   await initHive();
-  runApp(const MyApp());
+
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
